8083e6e8e562164270d4b29ac8506ca27271a426
Annelin <annelinruba@gmail.com>
2026-07-25 13:49:45 +0530

    Update imports and Flask app initialization
    Fix import statements and correct Flask initialization.

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
+import os , re , urlib.request , urlib.parse
+from Flask import flask, jsonify, request, Request
 
+app = falsk(__name__)
+req = urlib.request(f"https://www.youtube.com/search?" methods = ("agents", Mozilla/5.0))
+re = url 
